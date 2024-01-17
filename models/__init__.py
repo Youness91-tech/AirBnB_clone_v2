f4080a366c76837bedac2e3cc77fd20a0f431746
Youness91-Tech <bayttarbawi.com@gmail.com>
2024-01-17 11:58:06 -0800

    __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Create a unique storage instance for your application"""
+
+from os import getenv
+
+
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
