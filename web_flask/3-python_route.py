1fd76b0a11d6408b61c9578205b8586ad5937681
Youness91-tech <bayttarbawi.com@gmail.com>
2024-02-16 13:05:38 -0800

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Starts a Flask web application:
+    - web application must be listening on 0.0.0.0, port 5000
+    - Routes:
+        - /: display "Hello HBNB!"
+        - /hbnb: display "HBNB"
+        - /c/<text>: display "C" followed by the value of the text variable
+        - /python/(<text>): display "Python", followed by the value of the
+            text variable
+            - The default value of text is "is cool"
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """display "Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display "HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """display C followed by value of the <text> variable"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is cool'):
+    """display python followed by value of the <text> variable"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
